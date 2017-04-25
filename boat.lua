7016c7c5e796174eb718cb7434228dcf188d7b4d
Centralan <Jessegauthier070296@gmail.com>
2017-04-25 15:52:02 -0400

    removed test ai message

--- boat.lua
+++ boat.lua
@@ -18,15 +18,6 @@ function a_whisper_npc(npc, msg, player)
 	player:sendMessage('&b[BW]&7Captain Flint &b' .. npc .. ' &3-> &f' .. msg);
 end
 
-function aiflint_enter(data)
-        local player = Player:new(data.player);
-        a_whisper_npc(flint, "Welcome to Boat Wars! - Test", player);
-         soundblock:playSound('NOTE_PLING', 1000, 10);
-         soundblock:playSound('ORB_PICKUP', 1000, 10);
-end
-
-registerHook("REGION_ENTER", "bw-enter", "aiflint_enter");
-
 --PvP
 --
 
