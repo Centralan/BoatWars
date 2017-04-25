7d2daf0e22b5f875df4f7ee1b93fb1b23c6ea760
Centralan <Jessegauthier070296@gmail.com>
2017-04-24 21:24:35 -0400

    First Commit, basic creation of AI with test message.

---
+++ boat.lua
@@ -0,0 +1,28 @@
+-- AI
+--
+
+local world = World:new('BoatWars');
+local soundblock = Location:new(world, 0, 100, 0);
+local ai = 'Captain Flint'
+local Rules = ''
+
+function a_broadcast(msg)
+	world:broadcast(msg);
+end
+
+function a_broadcast_npc(npc, msg)
+	a_broadcast('&f &b' .. npc .. '&f: ' .. msg);
+end
+
+function a_whisper_npc(npc, msg, player)
+	player:sendMessage('&b[BW] &b' .. npc .. ' &3-> &f' .. msg);
+end
+
+function aiflint_enter(data)
+        local player = Player:new(data.player);
+        a_whisper_npc(Captain Flint, "&6Welcome to Boat Wars!", player);
+         soundblock:playSound('NOTE_PLING', 1000, 10);
+         soundblock:playSound('ORB_PICKUP', 1000, 10);
+end
+
+registerHook("REGION_ENTER", "bw-enter", "aiflint_enter");
