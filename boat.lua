8db422db09741e08835247030d9099c76ce03b3e
Centralan <Jessegauthier070296@gmail.com>
2017-04-24 21:32:50 -0400

    Updated AI text

--- boat.lua
+++ boat.lua
@@ -3,7 +3,7 @@
 
 local world = World:new('BoatWars');
 local soundblock = Location:new(world, 0, 100, 0);
-local ai = 'Captain Flint'
+local ai = 'flint'
 local Rules = ''
 
 function a_broadcast(msg)
@@ -15,12 +15,12 @@ function a_broadcast_npc(npc, msg)
 end
 
 function a_whisper_npc(npc, msg, player)
-	player:sendMessage('&b[BW] &b' .. npc .. ' &3-> &f' .. msg);
+	player:sendMessage('&b[BW]&7Captain Flint &b' .. npc .. ' &3-> &f' .. msg);
 end
 
 function aiflint_enter(data)
         local player = Player:new(data.player);
-        a_whisper_npc(Captain Flint, "&6Welcome to Boat Wars!", player);
+        a_whisper_npc(flint, "Welcome to Boat Wars! - Test", player);
          soundblock:playSound('NOTE_PLING', 1000, 10);
          soundblock:playSound('ORB_PICKUP', 1000, 10);
 end
